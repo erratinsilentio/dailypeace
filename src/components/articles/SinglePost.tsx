c57f85b0e5c09a0faaea26afa7f40e8c9b3e127e
erratinsilentio <kacper.kondracki@wp.pl>
2025-05-01 14:33:37 +0200

    mobile for blog page complete

--- src/components/articles/SinglePost.tsx
+++ src/components/articles/SinglePost.tsx
@@ -1,11 +1,12 @@
+import { BookOpen } from "lucide-react";
 import DateFormat from "../DateFormat";
 
 export default function SinglePost({post}) {
     return (
-            <article className="flex flex-row w-[800px] h-[240px]" key={post.slug}>
-                <img src={post.data.heroImage} alt="postcard" className="w-[240px] aspect-square" />
-                <div className="flex flex-col px-8 py-2">
-                    <span className="flex flex-row items-center justify-start">
+            <article className="flex flex-col md:flex-row md:w-[800px] md:h-[240px] px-6 md:px-0 rounded-2xl hover:cursor-pointer group" key={post.slug}>
+                <img src={post.data.heroImage} alt="postcard" className="w-full md:w-[240px] aspect-square dark:shadow-violet-100 dark:shadow-xs" />
+                <div className="flex flex-col mx-4 md:px-8 pt-4 md:py-2">
+                    <span className="flex flex-row items-center justify-between md:justify-start pt-4">
                         <span className="text-stone-500 pr-6 text-[14px] dark:text-stone-200">
                             <DateFormat date={post.data.pubDate} />
                         </span>
@@ -15,8 +16,9 @@ export default function SinglePost({post}) {
                     </span>
                     <span className="text-[20px] py-2 dark:text-white">{post.data.title}</span>
                     <span className="text-stone-600 text-[16px] dark:text-stone-300 font-extralight">{post.data.description}</span>
-                    <a href={`/articles/${post.id}/`}  className="py-4">
-                        <span className="text-[18px] text-stone-900 hover:underline dark:text-white">Read More...</span>
+                    
+                    <a href={`/articles/${post.id}/`}  className="py-4 pt-8">
+                        <span className="text-[18px] text-stone-900 underline dark:text-white opacity-75 group-hover:opacity-100 font-[Indie_Flower] flex flex-row items-center gap-3"><BookOpen /> Read More...</span>
                     </a>
                 </div>
             </article>
